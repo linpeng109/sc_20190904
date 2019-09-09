2c3f51bb763d9cc3855e4a10f2fcd560bf6b8ba0
linpeng109 <linpeng109@aliyun.com>
2019-09-10 05:23:22 +0800

    add ftp server

--- userBaseServer1/src/main/java/com/cn/ftp/MyFtpLet.java
+++ userBaseServer1/src/main/java/com/cn/ftp/MyFtpLet.java
@@ -1,19 +1,23 @@
 package com.cn.ftp;
 
 import lombok.Data;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.apache.ftpserver.ftplet.*;
 
 import java.io.IOException;
 
-@Data
+@Log4j2
 public class MyFtpLet extends DefaultFtplet {
     @Override
     public FtpletResult onLogin(FtpSession session, FtpRequest request) throws FtpException, IOException {
 //        获取上传文件信息
         String absolutePath = session.getFileSystemView().getHomeDirectory().getAbsolutePath();
+        log.debug(absolutePath);
         String filename = request.getArgument();
+        log.debug(filename);
         String homeDirectory = session.getUser().getHomeDirectory();
-        
+        log.debug(homeDirectory);
         return super.onLogin(session, request);
     }
 
