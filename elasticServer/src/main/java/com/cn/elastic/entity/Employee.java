d8289cf4a3b4ce41976cd4cade6c40801f63b70e
linpeng109 <linpeng109@aliyun.com>
2019-10-14 17:20:17 +0800

    I understand the lucene search

--- elasticServer/src/main/java/com/cn/elastic/entity/Employee.java
+++ elasticServer/src/main/java/com/cn/elastic/entity/Employee.java
@@ -6,6 +6,7 @@ import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -17,6 +18,8 @@ public class Employee {
     public String firstName;
     @Field(type = FieldType.Text)
     public String lastName;
+    @Field(type = FieldType.Date)
+    public Date createTime = new Date();
     @Field(type = FieldType.Text)
     public String company;
     @Field(type = FieldType.Text)
