3e1a126055b6012870eea7abc240ae89c0aaf168
linpeng109 <linpeng109@aliyun.com>
2019-11-20 17:32:25 +0800

    add integration kafka

--- elasticServer1/src/main/java/com/cn/integration/KafkaConsumer.java
+++ elasticServer1/src/main/java/com/cn/integration/KafkaConsumer.java
@@ -1,4 +1,4 @@
-package com.cn.kafka;
+package com.cn.integration;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
---
+++ integrationServer/src/main/java/com/cn/integration/kafka/KafkaIntegrationConfig.java
@@ -0,0 +1,8 @@
+package com.cn.integration.kafka;
+
+import org.springframework.context.annotation.ImportResource;
+
+@ImportResource(locations = {"classpath:spring-integration-kafka.xml"})
+public class KafkaIntegrationConfig {
+
+}
